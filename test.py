4782f573d2d24ca425f5dd0187b5a3b774d8b78e
Michael Julius <mjulius161@gmail.com>
2025-09-29 19:15:13 +0200

    testing nog een keer

--- test.py
+++ test.py
@@ -7,9 +7,10 @@ print(sorted_fruits)
 short_f = filter(lambda w: len(w) <=4, fruits)
 
 print(list(short_f))
+#test
 
 nums = [1,2,3,4,5]
 
 new_nums = map(lambda w: w * w if w % 2 != 0 else w, nums)
 
-print(list(new_nums))
+print(list(new_nums)) 
