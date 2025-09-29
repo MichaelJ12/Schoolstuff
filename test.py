ef0ee0e43280ee7144e358698b90fd3d69a87206
Michael Julius <mjulius161@gmail.com>
2025-09-29 15:46:47 +0200

    test

---
+++ test.py
@@ -0,0 +1,15 @@
+fruits = ['banana', 'apple', 'pear', 'kiwi', 'pineapple', 'mango']
+
+sorted_fruits = sorted(fruits, key=lambda w: len(w))
+
+print(sorted_fruits)
+
+short_f = filter(lambda w: len(w) <=4, fruits)
+
+print(list(short_f))
+
+nums = [1,2,3,4,5]
+
+new_nums = map(lambda w: w * w if w % 2 != 0 else w, nums)
+
+print(list(new_nums))
